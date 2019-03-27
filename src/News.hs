7c3c6ffe201e198c4c7f23d4452b2ac594eccc85
lnsf <lnsf03@gmail.com>
2019-03-27 23:04:36 +0900

    :+1: modify get param

--- src/News.hs
+++ src/News.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-module News (getTypingStrings) where
+module News (getArticlesFromApi) where
 
 import           Config
 import           Data.Aeson
@@ -26,6 +26,7 @@ getArticlesFromApi = do
     r <- setRequestSecure True
       . setRequestQueryString
         [ (B8.pack "country", Just (B8.pack "us"))
+        , (B8.pack "pageSize", Just (B8.pack "100"))
         , (B8.pack "apiKey", Just (B8.pack apiKey))]
       <$> parseRequest "https://newsapi.org/v2/top-headlines"
     httpLbs r
